f655f55a64ca6937e1b608037e60d495b1c4de7e
Mthokozisi Masondo <m.masondo2001@gmail.com>
2024-08-02 10:47:37 +0200

    Added player name and chips

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+    name: "Mthokozisi",
+    chips: 250
+}
 let cards = []
 let sum = 0
 let isAlive = false
@@ -6,6 +10,9 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": R" + player.chips 
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random() * 13 ) + 1
@@ -48,11 +55,13 @@ function renderGame() {
 }
 
 function newCard() {
+if (isAlive === true && hasBlackJack === false) {
     let card = getRandomCard()
     sum += card
     cards.push(card)
     console.log(cards)
     renderGame()
+    }
 }
 
 
